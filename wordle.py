584b248ea77d86f259347a32831d9a3efcbb1d1a
Annie Bryan <annieb22@mit.edu>
2022-06-16 14:24:40 +0200

    initial commit

---
+++ wordle.py
@@ -0,0 +1,12 @@
+from helper import remaining_words
+
+guesses = [
+    ("farce", "bygbb"),
+    ("straw", "bbgyb"),
+    ("xxxxx", "xxxxx"),
+    ("xxxxx", "xxxxx"),
+    ("xxxxx", "xxxxx"),
+    ("xxxxx", "xxxxx")
+]
+
+print(remaining_words(guesses))
