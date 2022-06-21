9bac85c727ed646efd0a3a23c36d8f10ef1881c3
Annie Bryan <annieb22@mit.edu>
2022-06-21 11:24:51 +0200

    Made interactive

--- wordle.py
+++ wordle.py
@@ -1,12 +1,21 @@
-from helper import remaining_words
+from helper import remaining_words, best_guesses
 
-guesses = [
-    ("farce", "bygbb"),
-    ("straw", "bbgyb"),
-    ("xxxxx", "xxxxx"),
-    ("xxxxx", "xxxxx"),
-    ("xxxxx", "xxxxx"),
-    ("xxxxx", "xxxxx")
-]
+def run():
+  guesses = []
+  finished = False
 
-print(remaining_words(guesses))
+  while len(guesses) < 6 and not finished:
+    guess = input("Guess: ")
+    result = input("Result: ")
+    guesses.append((guess, result))
+    rw = remaining_words(guesses)
+
+    if len(rw) == 1:
+      finished = True
+      print(f"The word must be: {rw[0]}")
+    else:
+      bg = best_guesses(rw, 5)
+      print(f"The word could be any of the following {len(rw)} words: {rw}")
+      print(f"The best choices for your next guess are: {bg}")
+
+run()
